f893bc3f63875efe9c7729580a31a1e97fc42c63
Ricardo Bravo <ricardobravo@bcp.com.pe>
2022-04-02 00:46:35 -0500

    feat: add deck class

--- deck.py
+++ deck.py
@@ -1,6 +1,20 @@
+from card import Card
 from constant import Constant
 
 class Deck:
 
     def __init__(self):
-        
+        self.all_cards = []
+
+        for suit in Constant.SUITS:
+            for rank in Constant.RANKS:
+                created_card = Card(suit, rank)
+
+                self.all_cards.append(created_card)
+
+def function():
+    new_deck = Deck()
+    print(new_deck.all_cards)
+
+if __name__ == "__main__":
+    function()                
