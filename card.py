b743e6596141bbd935b49e8165dc18eb4662a20a
Ricardo Bravo <ricardobravo@bcp.com.pe>
2022-04-02 00:40:55 -0500

    feat: add constant class

--- card.py
+++ card.py
@@ -1,16 +1,10 @@
-import random
-
-values = {'One': 1, 'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10, 'Eleven': 11, 'Twelve': 12, 'Thirtheen': 13}
-
-suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
-ranks = ('One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Eleven', 'Twelve', 'Thirtheen')
-
+from constant import Constant
 class Card:
 
     def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
-        self.value = values[rank]
+        self.value = Constant.VALUES[rank]
 
     def __str__(self):
         return self.rank + " of " + self.suit    
---
+++ deck.py
@@ -0,0 +1,6 @@
+from constant import Constant
+
+class Deck:
+
+    def __init__(self):
+        
